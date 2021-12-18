d2ebd9322c1b40323f03da78ea7946f54f1aa246
nickkuehl <47927264+nickkuehl@users.noreply.github.com>
2021-12-18 13:34:20 +0100

    DijkstraAlgorithm package created

---
+++ src/DijkstraAlgorithmus/Edge.java
@@ -0,0 +1,47 @@
+package DijkstraAlgorithmus;
+
+public class Edge {
+
+
+
+    private int weight;
+    public static int SumWeight;
+    private boolean isIncluded = false;
+    private boolean isPrinted = false;
+
+    public Edge(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isIncluded() {
+        return isIncluded;
+    }
+
+    public void setIncluded(boolean included) {
+        isIncluded = included;
+    }
+
+    public boolean isPrinted() {
+        return isPrinted;
+    }
+
+    public void setPrinted(boolean printed) {
+        isPrinted = printed;
+    }
+
+    public static int getSumWeight() {
+        return SumWeight;
+    }
+
+    public static void setSumWeight(int sumWeight) {
+        SumWeight = sumWeight;
+    }
+}
